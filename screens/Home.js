a2b4a6c3f9bb8c242bf210f61f466027c5ec03c8
Madhav Shukla <madhavshuklasr3@gmail.com>
2022-01-29 19:40:50 +0530

    google search and categories

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,17 @@
 import { View, Text,SafeAreaView,StatusBar } from 'react-native';
 import React from 'react';
 import HeaderTabs from '../components/HeaderTabs';
+import SearchBar from '../components/SearchBar';
+import Categories from '../components/Categories';
 
 export default function Home() {
   return (
-    <SafeAreaView style={{flex:1,marginTop:StatusBar.currentHeight}} >
-      <HeaderTabs />
+    <SafeAreaView style={{flex:1,marginTop:StatusBar.currentHeight,backgroundColor:"#eee"}} >
+      <View style={{backgroundColor:"white",padding:15}} >
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+      <Categories />
     </SafeAreaView>
   );
 }
